51f62fb179e4f06e4e30f54da534914f9cba6e40
KseniaShamina <126622222+KseniaShamina@users.noreply.github.com>
2023-07-12 19:32:16 -0400

    hw5

--- script.js
+++ script.js
@@ -29,18 +29,63 @@ function userAge() {
     const number = prompt("Введите число");
     if (number < 0 ) {
         console.log("Вы ввели неправильное значение");
-    } if else (number <=12) {
-        console.log("Привет, друг")
-    } if else (number >= 13) {
-        console.log("Добро пожаловать!")
+    } else if (number <=12) {
+        console.log("Привет, друг");
+    } else if (number >= 13) {
+        console.log("Добро пожаловать!");
     }
 } 
 
-function ckeckNum() {
+function checkNum() {
     const number = prompt("Введите число");
     if (number > 0) {
         return number**3
     } else {
-        console.log("Переданный параметр не является числом")
+        console.log("Переданный параметр не является числом");
+    }
+}
+
+
+function multiplyNumbers(a, b) {
+     if (isNaN(a) || isNaN(b)) {
+         console.log('Одно или оба значения не являются числом');
+     } else {
+         console.log(a * b);
+     }
+ }
+
+
+ const circle1 = {
+    radius: 8,
+    getArea() {
+       return 2 * Math.PI * this.radius; 
+    }
+    getPerimeter () {
+        return 2 * Math.PI * this.radius;
+    }
+  }
+
+  const circle2 = {
+    radius: 4,
+    getArea() {
+       return 2 * Math.PI * this.radius; 
+    }
+    getPerimeter () {
+        return 2 * Math.PI * this.radius;
+    }
+  }
+
+function GamesSeasons() {
+    let month = Number(prompt("Введите месяц"));
+    if (month===1 || month===2 || month===12) {
+        console.log("Зима");
+    } else if(month >= 3 && month < 6) {
+        console.log("Весна");
+    } else if(month >=7 && month < 9) {
+        console.log("лето");
+    } else if (month >=9 && month < 12) {
+        console.log("Зима");
+    } else {
+        alert("такого месяца не существует");
     }
 }
